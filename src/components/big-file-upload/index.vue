0aa5ce718a5bcd53f851f56b76ae075300799944
xiaozhen.quan <xiaozhen.quan@united-imaging.com>
2021-02-22 17:21:09 +0800

    带并发控制上传切片

--- src/components/big-file-upload/index.vue
+++ src/components/big-file-upload/index.vue
@@ -20,13 +20,22 @@ export default {
       default: () => 3
     }
   },
+  data() {
+    return {
+      sliceList: [],
+      uploadUrl: '/'
+    }
+  },
   methods: {
+    requset() {
+
+    },
     handleFile(file) {
       console.log(file)
       this.generateSlice(file)
     },
     generateSlice(file) {
-      let sliceList = []
+      let sliceList = this.sliceList
       let cur = 0
       let names = file.name.split('.')
       let fileName = names.splice(0, names.length - 1).join()
@@ -35,12 +44,43 @@ export default {
         let slice = {
           chunk: file.slice(cur, cur + this.sliceSize),
           name: fileName + '~' + sliceList.length + '.' + extension,
-          type: file.type
+          type: file.type,
+          index: sliceList.length
         }
         console.log(slice)
         sliceList.push(slice)
         cur += this.sliceSize
       }
+    },
+    uploadSlice() {
+      const requestList = this.sliceList.map(slice => {
+        const formData = new FormData()
+        formData.append('slice', slice)
+        return formData
+      }).map(data => {
+        this.requset(data)
+      })
+
+      this.concurrentRequest(requestList)
+    },
+    concurrentRequest(requestList) {
+      let curConcurren = 0
+      let datas = []
+      let index = 0
+      const _request = () => {
+        for (let i = index; i < requestList.length && curConcurren < this.concurrence; i++) {
+          curConcurren++
+          index++
+          requestList[i]().then((data) => {
+            curConcurren--
+            datas.splice(i, 0, data)
+            if (index < this.requestList.length) this._request()
+          }, (err) => {
+
+          })
+        }
+      }
+      _request()
     }
   }
 }
