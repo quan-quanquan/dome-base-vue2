80620a77fc0bbd07507c2077c299d73c93395b64
xiaozhen.quan <xiaozhen.quan@united-imaging.com>
2021-02-26 11:16:06 +0800

    resize 指令逻辑

---
+++ src/directives/resize.js
@@ -0,0 +1,39 @@
+function inserted(el, binding) {
+  const callback = binding.value
+
+  const debounce = binging.arg || 200
+
+  const options = binding.options || {passive: true}
+
+  let debounceTimeout = null
+
+  const onResize = () => {
+    clearTimeout(debounceTimeout)
+    debounceTimeout = setTimeout(callback, debounce, options)
+  }
+
+  window.addEventListener('resize', onResize, options)
+
+  el._onResize = {
+    callback,
+    options
+  }
+
+  if (!binding.modifiers || !binding.quiet) {
+    onResize()
+  }
+}
+
+function unbind(el, binding) {
+  const {callback, options} = el._onResize
+
+  window.removeEventListener('resize', callback, options)
+
+  delete el._onResize
+}
+
+export default {
+  name: 'resize',
+  inserted,
+  unbind
+}
